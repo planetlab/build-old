b2bd8345c10c4b4321f77049da20238bd64d18dc
Mark Huang <mlhuang@cs.princeton.edu>
2005-06-07 15:00:40 +0000

    PlanetLab Central Server Management 0.2-3
    ---++ plc-monitor
       * use new python scripts
    
    ---++ plc-www
       * execute backup as webmaster
       * remove assumptions about newlines existing in the key, don't print
         out anything for nodes that don't have key or ip/hostname.
    
    ---++ plc-cvs
       * allow ssh to cvs.planet-lab.org from anywhere

--- plc.mk
+++ plc.mk
@@ -4,7 +4,7 @@
 # Mark Huang <mlhuang@cs.princeton.edu>
 # Copyright (C) 2003-2005 The Trustees of Princeton University
 #
-# $Id: plc.mk,v 1.2.2.1 2005/05/17 21:33:51 mlhuang Exp $
+# $Id: plc.mk,v 1.2.2.2 2005/05/19 18:54:42 mlhuang Exp $
 #
 
 # Default target
@@ -30,8 +30,8 @@ all:
 #
 
 # Default values
-INITIAL := plc-0_2-2
-TAG := plc-0_2-2
+INITIAL := plc-0_2-3
+TAG := plc-0_2-3
 CVSROOT := :pserver:anon@cvs.planet-lab.org:/cvs
 
 #
