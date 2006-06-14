f5efcd7324025a3dc66cdf7b8df59120187a3b06
Steve Muir <smuir@cs.princeton.edu>
2006-06-14 18:05:58 +0000

    Add pl_auth to list of modules

--- planetlab.mk
+++ planetlab.mk
@@ -4,7 +4,7 @@
 # Mark Huang <mlhuang@cs.princeton.edu>
 # Copyright (C) 2003-2006 The Trustees of Princeton University
 #
-# $Id: planetlab.mk,v 1.20 2006/05/31 15:25:23 mlhuang Exp $
+# $Id: planetlab.mk,v 1.21 2006/06/01 20:52:57 mlhuang Exp $
 #
 
 #
@@ -254,6 +254,15 @@ ALL += util-python
 # proper and util-vserver both use scripts in util-python for building
 proper: util-python
 util-vserver: util-python
+pl_auth: util-python
+
+#
+# pl_auth
+#
+
+pl_auth-MODULE := pl_auth
+pl_auth-SPEC := pl_auth/pl_auth.spec
+ALL += pl_auth
 
 #
 # plcapilib
