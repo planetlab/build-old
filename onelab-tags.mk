c44c33d7532a2f9c21a0c67d7602cd563609c281
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2007-03-19 11:39:46 +0000

    oops - screwed up between package and MODULE

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -1,6 +1,5 @@
 # we do not use TAG directly anymore
 # this because we want the rpm's releases to reflect the date even when a tag is used
- 
 
 CVSROOT := :pserver:anon@cvs.planet-lab.org:/cvs
 
@@ -9,7 +8,18 @@ CVSROOT := :pserver:anon@cvs.planet-lab.org:/cvs
 # typically when then use mkfedora, and for that purpose they use the following tag.
 build-TAG :=			planetlab-4_0-rc2
 
+# the build's logic is sometimes confusing. Whether a package mentions a single or multiple modules, 
+# the variable used is different. 
+# Look for xxx-ambiguous-xxx below
+# We define both just in case (as of now only the first one is used)
+
+# xxx-ambiguous-xxx
 linux-2.6-TAG :=		planetlab-4_0-rc2
+kernel-i686-TAG :=		planetlab-4_0-rc2
+# we dont use this yet
+kernel-x86_64-TAG :=		planetlab-4_0-rc2
+kernel-i586-TAG :=		planetlab-4_0-rc2
+# end
 vnet-TAG :=			planetlab-4_0-rc2
 madwifi-ng-TAG := 		planetlab-4_0-rc2
 wireless-tools-SVNPATH :=	svn+ssh://build@svn.one-lab.org/svn/wireless-tools/tags/29pre14
@@ -35,13 +45,13 @@ iptables-TAG := 		planetlab-4_0-rc2
 iproute2-TAG := 		planetlab-4_0-rc2
 kexec-tools-TAG := 		planetlab-4_0-rc2
 util-python-TAG := 		planetlab-4_0-rc2
-# the build's logic is sometimes confusing. Whether a package mentions a single or multiple modules, 
-# the variable used is different. We define both just in case (as of now only the first one is used)
+# xxx-ambiguous-xxx
 PLCAPI-SVNPATH :=		svn+ssh://build@svn.one-lab.org/svn/new_plc_api/trunk
 new_plc_api-SVNPATH :=		svn+ssh://build@svn.one-lab.org/svn/new_plc_api/trunk
+# end
 vserver-reference-TAG := 	planetlab-4_0-rc2
 bootmanager-SVNPATH :=		svn+ssh://build@svn.one-lab.org/svn/bootmanager/trunk
 bootcd-SVNPATH :=		svn+ssh://build@svn.one-lab.org/svn/bootcd/trunk
-myplc-TAG := 			planetlab-4_0-rc2
+myplc-SVNPATH :=		svn+ssh://build@svn.one-lab.org/svn/myplc/trunk
 new_plc_www-TAG := 		planetlab-4_0-rc2
 plc/scripts-TAG := 		planetlab-4_0-rc2
