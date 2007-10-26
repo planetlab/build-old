44e8188bbe0b6683cd9bd9a8ca18e7b482d1e7e2
Marc Fiuczynski <mef@cs.princeton.edu>
2007-10-26 21:09:48 +0000

    pass in distro and release number to the build for myplc-devel-native

--- planetlab.mk
+++ planetlab.mk
@@ -58,7 +58,8 @@ endif
 
 # Figure out whether we are building on i386 or x86_64 host
 HOSTARCH := $(shell uname -i)
-FEDORA := $(shell ./getrelease.sh)
+DISTRO := $(shell ./getdistro.sh)
+RELEASE := $(shell ./getrelease.sh)
 
 kernel-$(HOSTARCH)-MODULE := Linux-2.6
 kernel-$(HOSTARCH)-SPEC := scripts/kernel-2.6-planetlab.spec
@@ -412,7 +413,7 @@ MyPLC-devel-RPMBUILD := sudo bash ./rpmbuild.sh
 #
 
 MyPLC-devel-native-MODULE := MyPLC
-MyPLC-devel-native-RPMFLAGS := --define "fedora $(FEDORA)"
+MyPLC-devel-native-RPMFLAGS := --define "distro $(DISTRO)" --define "release $(RELEASE)"
 MyPLC-devel-native-SPEC := myplc-devel-native.spec
 ALL += MyPLC-devel-native
 
