fa469b6d422a4774d604a9197c9129dcd7d61627
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2007-11-27 12:40:56 +0000

    * Makefile mostly reverted to rev. 7084 - do not use srpm packages for now * switched back to 2.6.22 for f8 only - no need to try this anyway * re-enabled proper

--- planetlab.mk
+++ planetlab.mk
@@ -20,8 +20,7 @@
 # in this case the srpm is created by running make srpm in the codebase
 #
 
-# using 2.6.22 on all platforms - experimental
-#ifeq "$(RELEASE)" "8"
+ifeq "$(RELEASE)" "8"
 srpm-kernel-$(HOSTARCH)-MODULES := linux-patches
 srpm-kernel-$(HOSTARCH)-SPEC := kernel-2.6-planetlab.spec
 ifeq ($(HOSTARCH),i386)
@@ -31,16 +30,16 @@ srpm-kernel-$(HOSTARCH)-RPMFLAGS:= --target $(HOSTARCH)
 endif
 KERNELS += srpm-kernel-$(HOSTARCH)
 
-#else
-#kernel-$(HOSTARCH)-MODULES := linux-tree
-#kernel-$(HOSTARCH)-SPEC := scripts/kernel-2.6-planetlab.spec
-#ifeq ($(HOSTARCH),i386)
-#kernel-$(HOSTARCH)-RPMFLAGS:= --target i686
-#else
-#kernel-$(HOSTARCH)-RPMFLAGS:= --target $(HOSTARCH)
-#endif
-#KERNELS += kernel-$(HOSTARCH)
-#endif
+else
+kernel-$(HOSTARCH)-MODULES := linux-tree
+kernel-$(HOSTARCH)-SPEC := scripts/kernel-2.6-planetlab.spec
+ifeq ($(HOSTARCH),i386)
+kernel-$(HOSTARCH)-RPMFLAGS:= --target i686
+else
+kernel-$(HOSTARCH)-RPMFLAGS:= --target $(HOSTARCH)
+endif
+KERNELS += kernel-$(HOSTARCH)
+endif
 
 kernel: $(KERNELS)
 kernel-clean: $(foreach package,$(KERNELS),$(package)-clean)
@@ -113,8 +112,7 @@ proper-SPEC := proper.spec
 proper-RPMBUILD := sudo bash ./rpmbuild.sh
 # proper uses scripts in util-python for building
 proper-DEPENDS := libhttpd++ util-python
-# disabled temporarily (or ?)
-#ALL += proper
+ALL += proper
 
 #
 # codemux: Port 80 demux
