a0c41cdebb867cb504f23a3e7520344a1cea1c29
Marc Fiuczynski <mef@cs.princeton.edu>
2007-11-27 17:04:30 +0000

    revert to Thierry's srpm build approach, and change DEPENDS to kernel-devel

--- planetlab.mk
+++ planetlab.mk
@@ -20,7 +20,6 @@
 # in this case the srpm is created by running make srpm in the codebase
 #
 
-ifeq "$(RELEASE)" "8"
 srpm-kernel-$(HOSTARCH)-MODULES := linux-patches
 srpm-kernel-$(HOSTARCH)-SPEC := kernel-2.6-planetlab.spec
 ifeq ($(HOSTARCH),i386)
@@ -30,19 +29,22 @@ srpm-kernel-$(HOSTARCH)-RPMFLAGS:= --target $(HOSTARCH)
 endif
 KERNELS += srpm-kernel-$(HOSTARCH)
 
-else
-kernel-$(HOSTARCH)-MODULES := linux-tree
-kernel-$(HOSTARCH)-SPEC := scripts/kernel-2.6-planetlab.spec
-ifeq ($(HOSTARCH),i386)
-kernel-$(HOSTARCH)-RPMFLAGS:= --target i686
-else
-kernel-$(HOSTARCH)-RPMFLAGS:= --target $(HOSTARCH)
-endif
-KERNELS += kernel-$(HOSTARCH)
-endif
+#else
+#kernel-$(HOSTARCH)-MODULES := linux-tree
+#kernel-$(HOSTARCH)-SPEC := scripts/kernel-2.6-planetlab.spec
+#ifeq ($(HOSTARCH),i386)
+#kernel-$(HOSTARCH)-RPMFLAGS:= --target i686
+#else
+#kernel-$(HOSTARCH)-RPMFLAGS:= --target $(HOSTARCH)
+#endif
+#KERNELS += kernel-$(HOSTARCH)
+#endif
 
 kernel: $(KERNELS)
 kernel-clean: $(foreach package,$(KERNELS),$(package)-clean)
+
+kernel-devel: $(KERNELS)
+	sudo rpm -Uvh RPMS/i686/kernel-devel*.rpm
 
 ALL += $(KERNELS)
 
@@ -112,7 +114,8 @@ proper-SPEC := proper.spec
 proper-RPMBUILD := sudo bash ./rpmbuild.sh
 # proper uses scripts in util-python for building
 proper-DEPENDS := libhttpd++ util-python
-ALL += proper
+# disabled temporarily (or ?)
+#ALL += proper
 
 #
 # codemux: Port 80 demux
@@ -127,7 +130,7 @@ ALL += codemux
 #
 ulogd-MODULES := ulogd
 ulogd-SPEC := ulogd.spec
-ulogd-DEPENDS := $(KERNELS) proper
+ulogd-DEPENDS := kernel-devel proper
 ALL += ulogd
 
 #
@@ -156,7 +159,7 @@ ALL += pl_mom
 #
 iptables-MODULES := iptables
 iptables-SPEC := iptables.spec
-iptables-DEPENDS := $(KERNELS)
+iptables-DEPENDS :=kernel-devel
 ALL += iptables
 
 #
