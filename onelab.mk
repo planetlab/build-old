555f69c4c98234d3e3da16ae3c71b0a1d44fd3f8
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-01-10 18:35:51 +0000

    this is needed too

--- onelab.mk
+++ onelab.mk
@@ -84,8 +84,8 @@ nozomi-DEPEND-DEVEL-RPMS := kernel-devel
 nozomi-SPECVARS = kernel_version=$(kernel.rpm-version) \
 	kernel_release=$(kernel.rpm-release) \
 	kernel_arch=$(kernel.rpm-arch)
-#IN_BOOTSTRAPFS += nozomi
-#ALL += nozomi
+IN_BOOTSTRAPFS += nozomi
+ALL += nozomi
 
 #
 # comgt
