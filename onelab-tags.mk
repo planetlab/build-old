248eb419ae6cc283f88ae7d267b7056b98bdff3f
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-01-10 13:48:37 +0000

    tags&trunk on onelab - same as with planetlab

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -1,40 +1,33 @@
 # $Id: planetlab-tags.mk 7709 2008-01-08 20:32:49Z thierry $
 
 build-SVNPATH			:= http://svn.planet-lab.org/svn/build/trunk
-linux-patches-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-22-1
-madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-0.9.3.3-1
-wireless-tools-SVNPATH		:= http://svn.planet-lab.org/svn/wireless-tools/tags/wireless-tools-29-0
+linux-patches-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/trunk
+madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/trunk
+wireless-tools-SVNPATH		:= http://svn.planet-lab.org/svn/wireless-tools/trunk
 nozomi-SVNPATH			:= http://svn.one-lab.org/nozomi/imports/2.21alpha_060917
 comgt-SVNPATH			:= http://svn.one-lab.org/comgt/imports/0.3
-libnl-SVNPATH 			:= http://svn.planet-lab.org/svn/libnl/tags/libnl-1.0-11
+libnl-SVNPATH 			:= http://svn.planet-lab.org/svn/libnl/trunk
 util-vserver-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver/branches/scholz
-util-vserver-pl-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver-pl/tags/util-vserver-pl-0.1-1
-NodeUpdate-SVNPATH		:= http://svn.planet-lab.org/svn/NodeUpdate/tags/NodeUpdate-0.5-2
-PingOfDeath-SVNPATH		:= http://svn.planet-lab.org/svn/PingOfDeath/tags/ipod-2.2-1
-NodeManager-SVNPATH		:= http://svn.planet-lab.org/svn/NodeManager/tags/NodeManager-1.6-1
-pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn/pl_sshd/tags/pl_sshd-1.0-10
-libhttpd++-SVNPATH		:= http://svn.planet-lab.org/svn/libhttpd++/tags/libhttpd++-0.1.4-1
-proper-SVNPATH			:= http://svn.planet-lab.org/svn/proper/tags/proper-0.3-37
-CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn/CoDemux/tags/codemux-0.1-7
-ulogd-SVNPATH			:= http://svn.planet-lab.org/svn/ulogd/tags/ulogd-1.24-4
-fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn/fprobe-ulog/tags/fprobe-ulog-1.1.1-1
-PlanetFlow-SVNPATH		:= http://svn.planet-lab.org/svn/PlanetFlow/tags/netflow-4.0-1
-Mom-SVNPATH			:= http://svn.planet-lab.org/svn/Mom/tags/pl_mom-2.2-2
-iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.3.8-0
-iproute2-SVNPATH		:= http://svn.planet-lab.org/svn/iproute2/tags/iproute-2.6.16-1
-vsys-SVNPATH			:= http://svn.planet-lab.org/svn/vsys/tags/vsys-0.7-2
-PLCAPI-SVNPATH			:= http://svn.planet-lab.org/svn/PLCAPI/tags/PLCAPI-4.2-0
-WWW-SVNPATH			:= http://svn.one-lab.org/new_plc_www/tags/PLCWWW-4.2-1
-VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn/VserverReference/tags/vserver-4.2-0
-BootManager-SVNPATH		:= http://svn.planet-lab.org/svn/BootManager/tags/bootmanager-3.2-2
-pypcilib-SVNPATH		:= http://svn.planet-lab.org/svn/pypcilib/tags/pypcilib-0.1-1
-BootCD-SVNPATH			:= http://svn.planet-lab.org/svn/BootCD/tags/bootcd-3.4-2
-BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn/BootstrapFS/tags/bootstrapfs-0.1-1
-MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/tags/myplc-4.0-15
-
-#
-# unused so far
-# http://svn.planet-lab.org/svn/plcmdline/tags/plcapilib-0.1-5
-# http://svn.planet-lab.org/svn/util-python/tags/util-python-0.1-13
-# http://svn.planet-lab.org/svn/util-vserver/tags/util-vserver-0.30.214-1
-
+util-vserver-pl-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver-pl/trunk
+NodeUpdate-SVNPATH		:= http://svn.planet-lab.org/svn/NodeUpdate/trunk
+PingOfDeath-SVNPATH		:= http://svn.planet-lab.org/svn/PingOfDeath/trunk
+NodeManager-SVNPATH		:= http://svn.planet-lab.org/svn/NodeManager/trunk
+pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn/pl_sshd/trunk
+libhttpd++-SVNPATH		:= http://svn.planet-lab.org/svn/libhttpd++/trunk
+proper-SVNPATH			:= http://svn.planet-lab.org/svn/proper/trunk
+CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn/CoDemux/trunk
+ulogd-SVNPATH			:= http://svn.planet-lab.org/svn/ulogd/trunk
+fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn/fprobe-ulog/trunk
+PlanetFlow-SVNPATH		:= http://svn.planet-lab.org/svn/PlanetFlow/trunk
+Mom-SVNPATH			:= http://svn.planet-lab.org/svn/Mom/trunk
+iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/trunk
+iproute2-SVNPATH		:= http://svn.planet-lab.org/svn/iproute2/trunk
+vsys-SVNPATH			:= http://svn.planet-lab.org/svn/vsys/trunk
+PLCAPI-SVNPATH			:= http://svn.planet-lab.org/svn/PLCAPI/trunk
+WWW-SVNPATH			:= http://svn.one-lab.org/new_plc_www/trunk
+VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn/VserverReference/trunk
+BootManager-SVNPATH		:= http://svn.planet-lab.org/svn/BootManager/trunk
+pypcilib-SVNPATH		:= http://svn.planet-lab.org/svn/pypcilib/trunk
+BootCD-SVNPATH			:= http://svn.planet-lab.org/svn/BootCD/trunk
+BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn/BootstrapFS/trunk
+MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/trunk
