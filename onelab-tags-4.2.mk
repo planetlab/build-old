788b580c6952382d6db28e644ac80c1d7f4bd8f7
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-01-14 14:40:38 +0000

    should have been done by module-tag at the time we moved to 4.2

--- onelab-tags-4.2.mk
+++ onelab-tags-4.2.mk
@@ -30,4 +30,4 @@ BootManager-SVNPATH		:= http://svn.planet-lab.org/svn/BootManager/tags/bootmanag
 pypcilib-SVNPATH		:= http://svn.planet-lab.org/svn/pypcilib/tags/pypcilib-0.1-1
 BootCD-SVNPATH			:= http://svn.planet-lab.org/svn/BootCD/tags/bootcd-3.4-2
 BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn/BootstrapFS/tags/bootstrapfs-0.1-1
-MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/tags/myplc-4.0-15
+MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/tags/myplc-4.2.0
