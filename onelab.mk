a5ae7c4d0fe770ed34fdf238660c8c85140bc498
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-01-29 10:24:22 +0000

    iwlwifi embedded into the kernel

--- onelab.mk
+++ onelab.mk
@@ -66,19 +66,6 @@ ALL += madwifi
 IN_BOOTSTRAPFS += madwifi
 
 #
-# iwlwifi
-#
-iwlwifi-MODULES := iwlwifi
-iwlwifi-SPEC := iwlwifi.spec
-iwlwifi-BUILD-FROM-SRPM := yes
-iwlwifi-DEPEND-DEVEL-RPMS := kernel-devel mac80211-devel
-iwlwifi-SPECVARS = kernel_version=$(kernel.rpm-version) \
-	kernel_release=$(kernel.rpm-release) \
-	kernel_arch=$(kernel.rpm-arch)
-#ALL += iwlwifi
-#IN_BOOTSTRAPFS += iwlwifi
-
-#
 # wireless-tools
 # 
 wireless-tools-MODULES := wireless-tools
