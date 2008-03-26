831f83b1731a34cb7221e4fc46d303ce26304d69
Stephen Soltesz <soltesz@cs.princeton.edu>
2008-03-26 18:27:53 +0000

    Updated all packages that do not yet have a current tag to pull from trunk. This will allow the RC to build successfully until new tags are created for the remaining packages

--- planetlab-tags-4.2.mk
+++ planetlab-tags-4.2.mk
@@ -1,28 +1,28 @@
 # $Id$
 
 build-SVNPATH			:= http://svn.planet-lab.org/svn/build/trunk
-linux-patches-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-22-1
+NodeManager-SVNPATH		:= http://svn.planet-lab.org/svn/NodeManager/trunk
+pf2slice-SVNPATH                := http://svn.planet-lab.org/svn/pf2slice/trunk
+fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn/fprobe-ulog/trunk
+iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/trunk
+vsys-SVNPATH			:= http://svn.planet-lab.org/svn/vsys/trunk
+linux-patches-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/trunk
+CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn/CoDemux/trunk
+util-vserver-pl-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver-pl/trunk
 madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-0.9.3.3-1
 wireless-tools-SVNPATH		:= http://svn.planet-lab.org/svn/wireless-tools/tags/wireless-tools-29-0
 libnl-SVNPATH 			:= http://svn.planet-lab.org/svn/libnl/tags/libnl-1.0-11
 #util-vserver-SVNPATH		:= http://svn.linux-vserver.org/svn/util-vserver/tags/release-0.30.215
 util-vserver-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver/branches/scholz
-util-vserver-pl-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver-pl/tags/util-vserver-pl-0.1-1
 NodeUpdate-SVNPATH		:= http://svn.planet-lab.org/svn/NodeUpdate/tags/NodeUpdate-0.5-2
 PingOfDeath-SVNPATH		:= http://svn.planet-lab.org/svn/PingOfDeath/tags/PingOfDeath-2.2-1
-NodeManager-SVNPATH		:= http://svn.planet-lab.org/svn/NodeManager/tags/NodeManager-1.7-2
 pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn/pl_sshd/tags/pl_sshd-1.0-10
 libhttpd++-SVNPATH		:= http://svn.planet-lab.org/svn/libhttpd++/tags/libhttpd++-0.1.4-1
 proper-SVNPATH			:= http://svn.planet-lab.org/svn/proper/tags/proper-0.3-37
-CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn/CoDemux/tags/CoDemux-0.1-7
 ulogd-SVNPATH			:= http://svn.planet-lab.org/svn/ulogd/tags/ulogd-1.24-4
-fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn/fprobe-ulog/tags/fprobe-ulog-1.1.2-1
-pf2slice-SVNPATH                := http://svn.planet-lab.org/svn/pf2slice/trunk
 PlanetFlow-SVNPATH		:= http://svn.planet-lab.org/svn/PlanetFlow/tags/PlanetFlow-4.0-3
 Mom-SVNPATH			:= http://svn.planet-lab.org/svn/Mom/tags/Mom-2.2-2
-iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.3.8-0
 iproute2-SVNPATH		:= http://svn.planet-lab.org/svn/iproute2/tags/iproute2-2.6.16-1
-vsys-SVNPATH			:= http://svn.planet-lab.org/svn/vsys/tags/vsys-0.7-3
 PLCAPI-SVNPATH			:= http://svn.planet-lab.org/svn/PLCAPI/tags/PLCAPI-4.2-4
 PLCWWW-SVNPATH			:= http://svn.planet-lab.org/svn/PLCWWW/tags/PLCWWW-4.2-1
 nodeconfig-SVNPATH		:= http://svn.planet-lab.org/svn/nodeconfig/tags/nodeconfig-4.2-2
