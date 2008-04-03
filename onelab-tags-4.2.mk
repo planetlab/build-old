715d7b657b32d2ff7ef6a9bd25e2397f0c75e343
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-04-03 09:02:51 +0000

    sync with the planetlab distro

--- onelab-tags-4.2.mk
+++ onelab-tags-4.2.mk
@@ -9,26 +9,32 @@ nozomi-SVNPATH			:= http://svn.one-lab.org/nozomi/tags/nozomi-2.21-1
 comgt-SVNPATH			:= http://svn.one-lab.org/comgt/imports/0.3
 libnl-SVNPATH 			:= http://svn.planet-lab.org/svn/libnl/tags/libnl-1.0-11
 util-vserver-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver/tags/util-vserver-0.30.215-2
-util-vserver-pl-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver-pl/tags/util-vserver-pl-0.1-1
+# not stable yet
+util-vserver-pl-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver-pl/trunk
 NodeUpdate-SVNPATH		:= http://svn.planet-lab.org/svn/NodeUpdate/tags/NodeUpdate-0.5-2
 PingOfDeath-SVNPATH		:= http://svn.planet-lab.org/svn/PingOfDeath/tags/PingOfDeath-2.2-1
-NodeManager-SVNPATH		:= http://svn.planet-lab.org/svn/NodeManager/tags/NodeManager-1.7-3
+NodeManager-SVNPATH		:= http://svn.planet-lab.org/svn/NodeManager/tags/NodeManager-1.7-4
 pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn/pl_sshd/tags/pl_sshd-1.0-10
 libhttpd++-SVNPATH		:= http://svn.planet-lab.org/svn/libhttpd++/tags/libhttpd++-0.1.4-1
 proper-SVNPATH			:= http://svn.planet-lab.org/svn/proper/tags/proper-0.3-37
-CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn/CoDemux/tags/CoDemux-0.1-8
+# not stable yet
+CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn/CoDemux/trunk
 ulogd-SVNPATH			:= http://svn.planet-lab.org/svn/ulogd/tags/ulogd-1.24-4
-fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn/fprobe-ulog/tags/fprobe-ulog-1.1.2-1
+# not stable yet
+fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn/fprobe-ulog/trunk
+# not stable yet
 pf2slice-SVNPATH                := http://svn.planet-lab.org/svn/pf2slice/tags/pf2slice-1.0-1
 PlanetFlow-SVNPATH		:= http://svn.planet-lab.org/svn/PlanetFlow/tags/PlanetFlow-4.0-3
 Mom-branch			:= 2.2
 Mom-SVNPATH			:= http://svn.planet-lab.org/svn/Mom/tags/Mom-2.2-3
-iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.3.8-0
+# not stable yet
+iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/trunk
 iproute2-SVNPATH		:= http://svn.planet-lab.org/svn/iproute2/tags/iproute2-2.6.16-1
-vsys-SVNPATH			:= http://svn.planet-lab.org/svn/vsys/tags/vsys-0.7-3
+# not stable yet
+vsys-SVNPATH			:= http://svn.planet-lab.org/svn/vsys/trunk
 PLCAPI-SVNPATH			:= http://svn.planet-lab.org/svn/PLCAPI/tags/PLCAPI-4.2-4
 # need tagging tool
-#PLCWWW-SVNPATH			:= http://svn.planet-lab.org/svn/PLCWWW/tags/PLCWWW-4.2-8
+#PLCWWW-SVNPATH			:= http://svn.planet-lab.org/svn/PLCWWW/tags/PLCWWW-4.2-1
 PLCWWW-SVNPATH			:= http://svn.planet-lab.org/svn/PLCWWW/branches/onelab
 nodeconfig-SVNPATH		:= http://svn.planet-lab.org/svn/nodeconfig/tags/nodeconfig-4.2-2
 BootManager-SVNPATH		:= http://svn.planet-lab.org/svn/BootManager/tags/BootManager-3.2-5
