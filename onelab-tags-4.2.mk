617590c2536259dc6573ca490b9462af0ac61db2
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-04-09 09:50:12 +0000

    adopt latest myplc tag

--- onelab-tags-4.2.mk
+++ onelab-tags-4.2.mk
@@ -42,4 +42,4 @@ pypcilib-SVNPATH		:= http://svn.planet-lab.org/svn/pypcilib/tags/pypcilib-0.2-1
 BootCD-SVNPATH			:= http://svn.planet-lab.org/svn/BootCD/tags/BootCD-3.4-5
 VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn/VserverReference/tags/VserverReference-4.2-4
 BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn/BootstrapFS/tags/BootstrapFS-1.0-0
-MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.2-4
+MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.2-5
