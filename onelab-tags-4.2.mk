6c886f656e320fc6b95ddd37d2627abdcd512f48
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-04-21 13:04:22 +0000

    Tagging module VserverReference - VserverReference-4.2-5 tweaked pre script that was removing everything under /vservers/.vref dismantle vcached (as far as this module is concerned) sudo to log in /var/log/sudo

--- onelab-tags-4.2.mk
+++ onelab-tags-4.2.mk
@@ -38,6 +38,6 @@ nodeconfig-SVNPATH		:= http://svn.planet-lab.org/svn/nodeconfig/tags/nodeconfig-
 BootManager-SVNPATH		:= http://svn.planet-lab.org/svn/BootManager/tags/BootManager-3.2-5
 pypcilib-SVNPATH		:= http://svn.planet-lab.org/svn/pypcilib/tags/pypcilib-0.2-1
 BootCD-SVNPATH			:= http://svn.planet-lab.org/svn/BootCD/tags/BootCD-3.4-5
-VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn/VserverReference/tags/VserverReference-4.2-4
+VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn/VserverReference/tags/VserverReference-4.2-5
 BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn/BootstrapFS/tags/BootstrapFS-1.0-0
 MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.2-5
