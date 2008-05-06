12d8232f1bb874fe3b173c3a62bf1c35631206d8
Daniel Hokka Zakrisson <dhokka@cs.princeton.edu>
2008-05-06 20:16:12 +0000

    Tagging module linux-2.6 - linux-2.6-22-5 Patch needs to be applied.

--- planetlab-tags-4.2.mk
+++ planetlab-tags-4.2.mk
@@ -1,7 +1,7 @@
 # $Id$
 
 build-SVNPATH			:= http://svn.planet-lab.org/svn/build/trunk
-linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-22-4
+linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-22-5
 madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-0.9.3.3-1
 wireless-tools-SVNPATH		:= http://svn.planet-lab.org/svn/wireless-tools/tags/wireless-tools-29-0
 libnl-SVNPATH 			:= http://svn.planet-lab.org/svn/libnl/tags/libnl-1.0-11
