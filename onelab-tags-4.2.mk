eb14b33c57fec594df267e8fd6f5377f6ab16a38
Stephen Soltesz <soltesz@cs.princeton.edu>
2008-05-09 19:32:06 +0000

    Tagging module CoDemux - CoDemux-0.1-11

--- onelab-tags-4.2.mk
+++ onelab-tags-4.2.mk
@@ -19,7 +19,7 @@ PingOfDeath-SVNPATH		:= http://svn.planet-lab.org/svn/PingOfDeath/tags/PingOfDea
 NodeManager-SVNBRANCH		:= 1.7
 NodeManager-SVNPATH		:= http://svn.planet-lab.org/svn/NodeManager/tags/NodeManager-1.7-8
 pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn/pl_sshd/tags/pl_sshd-1.0-11
-CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn/CoDemux/tags/CoDemux-0.1-10
+CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn/CoDemux/tags/CoDemux-0.1-11
 fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn/fprobe-ulog/tags/fprobe-ulog-1.1.2-3
 # not stable yet
 pf2slice-SVNPATH                := http://svn.planet-lab.org/svn/pf2slice/tags/pf2slice-1.0-1
