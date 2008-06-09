cb99c763b7bbe21ea7e9af8e8ccc2795026a990f
Stephen Soltesz <soltesz@cs.princeton.edu>
2008-06-09 19:35:46 +0000

    make the *-tags-4.2 files point to the 4.2-rc6 tag of the build to create a reproducable build.

--- onelab-tags-4.2.mk
+++ onelab-tags-4.2.mk
@@ -4,7 +4,7 @@
 # but by modules.update to refresh modules.list 
 # that serves as the default for module-tools -a
 
-build-SVNPATH			:= http://svn.planet-lab.org/svn/build/trunk
+build-SVNPATH			:= http://svn.planet-lab.org/svn/build/tags/4.2-rc6
 linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-22-8
 madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-0.9.3.3-1
 wireless-tools-SVNPATH		:= http://svn.planet-lab.org/svn/wireless-tools/tags/wireless-tools-29-0
@@ -21,7 +21,6 @@ NodeManager-SVNPATH		:= http://svn.planet-lab.org/svn/NodeManager/tags/NodeManag
 pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn/pl_sshd/tags/pl_sshd-1.0-11
 CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn/CoDemux/tags/CoDemux-0.1-12
 fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn/fprobe-ulog/tags/fprobe-ulog-1.1.2-3
-# not stable yet
 pf2slice-SVNPATH                := http://svn.planet-lab.org/svn/pf2slice/tags/pf2slice-1.0-2
 Mom-SVNBRANCH			:= 2.2
 Mom-SVNPATH			:= http://svn.planet-lab.org/svn/Mom/tags/Mom-2.2-4
