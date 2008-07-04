8d01bdbb44eddbe0531e5e22ffd1ba60279ba731
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-07-04 00:20:30 +0000

    Tagging module MyPLC - MyPLC-4.2-16 yum config on nodes improved -- see https://svn.planet-lab.org/ticket/342 also does not create any repo under /var/www/html/install-rpms, to prevent conflicts with noderepo

--- planetlab-tags-4.2.mk
+++ planetlab-tags-4.2.mk
@@ -32,6 +32,6 @@ BootCD-SVNPATH                  := http://svn.planet-lab.org/svn/BootCD/tags/Boo
 VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn/VserverReference/tags/VserverReference-4.2-10
 BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn/BootstrapFS/tags/BootstrapFS-1.0-2
 MyPLC-SVNBRANCH			:= 4.2
-MyPLC-SVNPATH                   := http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.2-15
+MyPLC-SVNPATH                   := http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.2-16
 # locating the right test directory - see make testsvnpath
 TESTS_SVNPATH			:= http://svn.planet-lab.org/svn/tests/tags/tests-4.2-5
