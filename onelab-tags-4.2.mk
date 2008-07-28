7ca7ebb1e067f88b2140e43b945a8e8e8940e282
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-07-28 20:18:00 +0000

    Tagging module PLCAPI - PLCAPI-dummynet-8 change peering logs location under /var/log/peers/

--- onelab-tags-4.2.mk
+++ onelab-tags-4.2.mk
@@ -28,7 +28,7 @@ vsys-SVNPATH			:= http://svn.planet-lab.org/svn/vsys/tags/vsys-0.7-18
 # needs tagging
 dummynet_image-SVNPATH		:= http://svn.planet-lab.org/svn/dummynet_image/trunk
 PLCAPI-SVNBRANCH		:= dummynet
-PLCAPI-SVNPATH			:= http://svn.planet-lab.org/svn/PLCAPI/tags/PLCAPI-dummynet-7
+PLCAPI-SVNPATH			:= http://svn.planet-lab.org/svn/PLCAPI/tags/PLCAPI-dummynet-8
 PLCWWW-SVNBRANCH		:= onelab.4.2
 PLCWWW-SVNPATH			:= http://svn.planet-lab.org/svn/PLCWWW/tags/PLCWWW-onelab.4.2-14
 Monitor-SVNPATH			:= http://svn.planet-lab.org/svn/Monitor/tags/Monitor-1.0-2
