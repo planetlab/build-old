0e0751dd89954372553c0e28835f7092883e294b
Sapan Bhatia <sapanb@cs.princeton.edu>
2008-08-04 20:49:32 +0000

    Fixed a bug in my previous commit.
    Tagging module linux-2.6 - linux-2.6-22-23

--- onelab-tags-4.2.mk
+++ onelab-tags-4.2.mk
@@ -4,7 +4,7 @@
 # but by modules.update to refresh modules.list 
 # that serves as the default for module-tools -a
 
-linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-22-22
+linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-22-23
 madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-0.9.3.3-1
 nozomi-SVNPATH			:= http://svn.one-lab.org/nozomi/tags/nozomi-2.21-1
 comgt-SVNPATH			:= http://svn.one-lab.org/comgt/imports/0.3
