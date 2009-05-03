9363fff453386f1654f171bebf455cb9ca08175a
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2009-05-03 20:29:03 +0000

    use latest tags

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -3,7 +3,8 @@
 # build-SVNPATH is now set by vbuild-nightly.sh to avoid duplication
 
 linux-2.6-SVNBRANCH		:= 22
-linux-2.6-SVNPATH               := http://svn.planet-lab.org/svn//linux-2.6/tags/linux-2.6-22-33
+linux-2.6-SVNPATH               := http://svn.planet-lab.org/svn//linux-2.6/tags/linux-2.6-22-36
+madwifi-SVNBRANCH		:= 0.9.4
 madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-0.9.4-3
 nozomi-SVNPATH			:= http://svn.onelab.eu/nozomi/tags/nozomi-2.21-1
 comgt-SVNPATH			:= http://svn.onelab.eu/comgt/imports/0.3
@@ -21,8 +22,8 @@ pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn//pl_sshd/tags/pl_sshd-1.0-11
 CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn//CoDemux/tags/CoDemux-0.1-13
 fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn//fprobe-ulog/tags/fprobe-ulog-1.1.3-0
 pf2slice-SVNPATH		:= http://svn.planet-lab.org/svn//pf2slice/tags/pf2slice-1.0-2
-Mom-SVNPATH			:= http://svn.planet-lab.org/svn//Mom/tags/Mom-2.3-0
-iptables-SVNPATH		:= 1.3.8
+Mom-SVNPATH			:= http://svn.planet-lab.org/svn//Mom/tags/Mom-2.3-1
+iptables-SVNBRANCH		:= 1.3.8
 iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.3.8-9
 iproute2-SVNPATH		:= http://svn.planet-lab.org/svn//iproute2/tags/iproute2-2.6.16-2
 inotify-tools-SVNPATH		:= http://svn.planet-lab.org/svn//inotify-tools/tags/inotify-tools-3.13-2
@@ -36,12 +37,12 @@ PLEWWW-SVNPATH                  := http://svn.planet-lab.org/svn/PLEWWW/tags/PLE
 www-register-wizard-SVNPATH	:= http://svn.planet-lab.org/svn//www-register-wizard/tags/www-register-wizard-4.2-3
 Monitor-SVNPATH			:= http://svn.planet-lab.org/svn/Monitor/tags/Monitor-3.0-4
 nodeconfig-SVNPATH              := http://svn.planet-lab.org/svn/nodeconfig/tags/nodeconfig-4.3-2
-BootManager-SVNPATH		:= http://svn.planet-lab.org/svn//BootManager/tags/BootManager-4.3-5
+BootManager-SVNPATH		:= http://svn.planet-lab.org/svn//BootManager/tags/BootManager-4.3-6
 pypcilib-SVNPATH		:= http://svn.planet-lab.org/svn//pypcilib/tags/pypcilib-0.2-6
 BootCD-SVNPATH                  := http://svn.planet-lab.org/svn/BootCD/tags/BootCD-4.2-13
 VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn//VserverReference/tags/VserverReference-4.2-12
 BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn//BootstrapFS/tags/BootstrapFS-1.0-8
-MyPLC-SVNPATH                   := http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.3-5
+MyPLC-SVNPATH                   := http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.3-6
 
 # locating the right test directory - see make tests_svnpath
 TESTS_SVNPATH			:= http://svn.planet-lab.org/svn/tests/tags/tests-4.3-1
