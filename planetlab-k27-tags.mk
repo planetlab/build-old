d6d5fe8f6a039f4ce8e35fb5024e94c8bbde9383
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2009-05-16 13:54:17 +0000

    regular planetlab distro uses 2.6.22 - new tags file planetlab-k27-tags.mk for 2.6.27

--- planetlab-k27-tags.mk
+++ planetlab-k27-tags.mk
@@ -1,14 +1,10 @@
-# $Id$
+# $Id: planetlab-tags.mk 13481 2009-05-15 14:00:58Z thierry $
 
 # build-SVNPATH is now set by vbuild-nightly.sh to avoid duplication
 
-linux-2.6-SVNBRANCH		:= 22
-linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn//linux-2.6/tags/linux-2.6-22-38
-madwifi-SVNBRANCH		:= 0.9.4
-madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-0.9.4-3
-nozomi-SVNPATH			:= http://svn.onelab.eu/nozomi/tags/nozomi-2.21-1
-comgt-SVNPATH			:= http://svn.onelab.eu/comgt/imports/0.3
-planetlab-umts-tools-SVNPATH	:= http://svn.onelab.eu/planetlab-umts-tools/tags/planetlab-umts-tools-0.6-4
+# trying the 2.6.27 kernel
+linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-27-3
+madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-3878-0
 util-vserver-SVNBRANCH		:= scholz
 util-vserver-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver/tags/util-vserver-0.30.215-6
 libnl-SVNPATH			:= http://svn.planet-lab.org/svn/libnl/tags/libnl-1.1-2
@@ -23,14 +19,12 @@ CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn//CoDemux/tags/CoDemux-0.1-13
 fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn//fprobe-ulog/tags/fprobe-ulog-1.1.3-0
 pf2slice-SVNPATH		:= http://svn.planet-lab.org/svn//pf2slice/tags/pf2slice-1.0-2
 Mom-SVNPATH			:= http://svn.planet-lab.org/svn//Mom/tags/Mom-2.3-1
-iptables-SVNBRANCH		:= 1.3.8
-iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.3.8-9
+iptables-SVNPATH		:= http://svn.planet-lab.org/svn//iptables/tags/iptables-1.4.1.1-0
 iproute2-SVNPATH		:= http://svn.planet-lab.org/svn//iproute2/tags/iproute2-2.6.16-2
 inotify-tools-SVNPATH		:= http://svn.planet-lab.org/svn//inotify-tools/tags/inotify-tools-3.13-2
 vsys-SVNBRANCH			:= 0.9
-vsys-SVNPATH			:= http://svn.planet-lab.org/svn//vsys/tags/vsys-0.9-1
+vsys-SVNPATH			:= http://svn.planet-lab.org/svn/vsys/tags/vsys-0.9-1
 vsys-scripts-SVNPATH		:= http://svn.planet-lab.org/svn/vsys-scripts/tags/vsys-scripts-0.95-1
-dummynet_image-SVNPATH		:= http://svn.planet-lab.org/svn/dummynet_image/tags/dummynet_image-1.0-5
 PLCAPI-SVNPATH			:= http://svn.planet-lab.org/svn/PLCAPI/tags/PLCAPI-4.3-14
 drupal-SVNPATH			:= http://svn.planet-lab.org/svn/drupal/tags/drupal-4.7-11
 PLEWWW-SVNPATH			:= http://svn.planet-lab.org/svn/PLEWWW/tags/PLEWWW-4.3-13
--- planetlab-tags.mk
+++ planetlab-tags.mk
@@ -2,9 +2,10 @@
 
 # build-SVNPATH is now set by vbuild-nightly.sh to avoid duplication
 
-# trying the 2.6.27 kernel
-linux-2.6-SVNPATH               := http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-27-3
-madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-3878-0
+linux-2.6-SVNBRANCH		:= 22
+linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn//linux-2.6/tags/linux-2.6-22-38
+madwifi-SVNBRANCH		:= 0.9.4
+madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-0.9.4-3
 util-vserver-SVNBRANCH		:= scholz
 util-vserver-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver/tags/util-vserver-0.30.215-6
 libnl-SVNPATH			:= http://svn.planet-lab.org/svn/libnl/tags/libnl-1.1-2
@@ -18,25 +19,26 @@ pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn//pl_sshd/tags/pl_sshd-1.0-11
 CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn//CoDemux/tags/CoDemux-0.1-13
 fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn//fprobe-ulog/tags/fprobe-ulog-1.1.3-0
 pf2slice-SVNPATH		:= http://svn.planet-lab.org/svn//pf2slice/tags/pf2slice-1.0-2
-Mom-SVNPATH			:= http://svn.planet-lab.org/svn//Mom/tags/Mom-2.3-0
-iptables-SVNPATH		:= http://svn.planet-lab.org/svn//iptables/tags/iptables-1.4.1.1-0
+Mom-SVNPATH			:= http://svn.planet-lab.org/svn//Mom/tags/Mom-2.3-1
+iptables-SVNBRANCH		:= 1.3.8
+iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.3.8-9
 iproute2-SVNPATH		:= http://svn.planet-lab.org/svn//iproute2/tags/iproute2-2.6.16-2
 inotify-tools-SVNPATH		:= http://svn.planet-lab.org/svn//inotify-tools/tags/inotify-tools-3.13-2
-vsys-scripts-SVNPATH		:= http://svn.planet-lab.org/svn/vsys-scripts/tags/vsys-scripts-0.95-1
 vsys-SVNBRANCH			:= 0.9
 vsys-SVNPATH			:= http://svn.planet-lab.org/svn/vsys/tags/vsys-0.9-1
-PLCAPI-SVNPATH                  := http://svn.planet-lab.org/svn//PLCAPI/tags/PLCAPI-4.3-14
+vsys-scripts-SVNPATH		:= http://svn.planet-lab.org/svn/vsys-scripts/tags/vsys-scripts-0.95-1
+PLCAPI-SVNPATH			:= http://svn.planet-lab.org/svn/PLCAPI/tags/PLCAPI-4.3-14
 drupal-SVNPATH			:= http://svn.planet-lab.org/svn/drupal/tags/drupal-4.7-11
-PLEWWW-SVNPATH                  := http://svn.planet-lab.org/svn/PLEWWW/tags/PLEWWW-4.3-13
+PLEWWW-SVNPATH			:= http://svn.planet-lab.org/svn/PLEWWW/tags/PLEWWW-4.3-13
 www-register-wizard-SVNPATH	:= http://svn.planet-lab.org/svn//www-register-wizard/tags/www-register-wizard-4.2-3
 Monitor-SVNPATH			:= http://svn.planet-lab.org/svn/Monitor/tags/Monitor-3.0-8
-nodeconfig-SVNPATH              := http://svn.planet-lab.org/svn/nodeconfig/tags/nodeconfig-4.3-3
-BootManager-SVNPATH             := http://svn.planet-lab.org/svn//BootManager/tags/BootManager-4.3-7
+nodeconfig-SVNPATH		:= http://svn.planet-lab.org/svn/nodeconfig/tags/nodeconfig-4.3-3
+BootManager-SVNPATH		:= http://svn.planet-lab.org/svn//BootManager/tags/BootManager-4.3-7
 pypcilib-SVNPATH		:= http://svn.planet-lab.org/svn//pypcilib/tags/pypcilib-0.2-6
-BootCD-SVNPATH                  := http://svn.planet-lab.org/svn/BootCD/tags/BootCD-4.2-13
+BootCD-SVNPATH			:= http://svn.planet-lab.org/svn/BootCD/tags/BootCD-4.2-13
 VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn//VserverReference/tags/VserverReference-4.2-12
 BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn//BootstrapFS/tags/BootstrapFS-1.0-8
-MyPLC-SVNPATH                   := http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.3-10
+MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.3-10
 
 # locating the right test directory - see make tests_svnpath
-TESTS_SVNPATH			:= http://svn.planet-lab.org/svn/tests/tags/tests-4.3-1
+TESTS_SVNPATH			:= http://svn.planet-lab.org/svn/tests/trunk
