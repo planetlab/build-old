f2f41263886c09e06dc49df1f318e64da6cc9d8e
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2009-06-11 10:34:50 +0000

    new pldistro for on-node emulation - should be temporary

---
+++ embedemu-tags.mk
@@ -0,0 +1,52 @@
+# $Id: onelab-tags.mk 13823 2009-06-08 20:11:36Z thierry $
+
+# build-SVNPATH is now set by vbuild-nightly.sh to avoid duplication
+
+linux-2.6-SVNBRANCH		:= 22
+linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn//linux-2.6/tags/linux-2.6-22-39
+madwifi-SVNBRANCH		:= 0.9.4
+madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-0.9.4-3
+ipfw-SVNPATH			:= http://onelab1.iet.unipi.it/svn/ipfw/stable
+nozomi-SVNPATH			:= http://svn.onelab.eu/nozomi/tags/nozomi-2.21-1
+comgt-SVNPATH			:= http://svn.onelab.eu/comgt/imports/0.3
+planetlab-umts-tools-SVNPATH	:= http://svn.onelab.eu/planetlab-umts-tools/tags/planetlab-umts-tools-0.6-4
+util-vserver-SVNBRANCH		:= scholz
+util-vserver-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver/tags/util-vserver-0.30.215-6
+libnl-SVNPATH			:= http://svn.planet-lab.org/svn/libnl/tags/libnl-1.1-2
+util-vserver-pl-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver-pl/tags/util-vserver-pl-0.3-16
+NodeUpdate-SVNPATH		:= http://svn.planet-lab.org/svn//NodeUpdate/tags/NodeUpdate-0.5-4
+PingOfDeath-SVNPATH		:= http://svn.planet-lab.org/svn//PingOfDeath/tags/PingOfDeath-2.2-1
+NodeManager-SVNBRANCH		:= 1.8
+NodeManager-SVNPATH		:= http://svn.planet-lab.org/svn//NodeManager/tags/NodeManager-1.8-4
+pyplnet-SVNPATH			:= http://svn.planet-lab.org/svn//pyplnet/tags/pyplnet-4.3-3
+pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn//pl_sshd/tags/pl_sshd-1.0-11
+CoDemux-SVNPATH			:= http://svn.planet-lab.org/svn//CoDemux/tags/CoDemux-0.1-13
+fprobe-ulog-SVNPATH		:= http://svn.planet-lab.org/svn//fprobe-ulog/tags/fprobe-ulog-1.1.3-0
+pf2slice-SVNPATH		:= http://svn.planet-lab.org/svn//pf2slice/tags/pf2slice-1.0-2
+Mom-SVNPATH			:= http://svn.planet-lab.org/svn//Mom/tags/Mom-2.3-1
+iptables-SVNBRANCH		:= 1.3.8
+iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.3.8-9
+iproute2-SVNPATH		:= http://svn.planet-lab.org/svn//iproute2/tags/iproute2-2.6.16-2
+inotify-tools-SVNPATH		:= http://svn.planet-lab.org/svn//inotify-tools/tags/inotify-tools-3.13-2
+vsys-SVNBRANCH			:= 0.9
+vsys-SVNPATH			:= http://svn.planet-lab.org/svn//vsys/tags/vsys-0.9-3
+vsys-scripts-SVNPATH		:= http://svn.planet-lab.org/svn/vsys-scripts/tags/vsys-scripts-0.95-4
+dummynet_image-SVNPATH		:= http://svn.planet-lab.org/svn/dummynet_image/tags/dummynet_image-1.0-5
+PLCAPI-SVNPATH                  := http://svn.planet-lab.org/svn/PLCAPI/tags/PLCAPI-4.3-19
+drupal-SVNPATH			:= http://svn.planet-lab.org/svn/drupal/tags/drupal-4.7-12
+PLEWWW-SVNPATH                  := http://svn.planet-lab.org/svn/PLEWWW/tags/PLEWWW-4.3-18
+www-register-wizard-SVNPATH	:= http://svn.planet-lab.org/svn//www-register-wizard/tags/www-register-wizard-4.3-0
+Monitor-SVNPATH			:= http://svn.planet-lab.org/svn/Monitor/trunk
+nodeconfig-SVNPATH		:= http://svn.planet-lab.org/svn/nodeconfig/tags/nodeconfig-4.3-3
+BootManager-SVNPATH		:= http://svn.planet-lab.org/svn//BootManager/tags/BootManager-4.3-7
+pypcilib-SVNPATH		:= http://svn.planet-lab.org/svn//pypcilib/tags/pypcilib-0.2-7
+BootCD-SVNPATH			:= http://svn.planet-lab.org/svn/BootCD/tags/BootCD-4.2-13
+VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn//VserverReference/tags/VserverReference-4.2-12
+BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn//BootstrapFS/tags/BootstrapFS-1.0-8
+MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.3-13
+geniwrapper-SVNPATH             := http://svn.planet-lab.org/svn/geniwrapper/trunk
+pyopenssl-SVNPATH               := http://svn.planet-lab.org/svn/pyopenssl/tags/pyopenssl-0.9-1
+
+
+# locating the right test directory - see make tests_svnpath
+tests-SVNPATH                   := http://svn.planet-lab.org/svn/tests/tags/tests-4.3-2
