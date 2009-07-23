3f8e055b2356bebda4dd262ed05ce80e46e2b94e
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2009-07-23 20:55:55 +0000

    Setting tag geniwrapper-0.8-6 snapshot after the GEC5 demo should be the last tag set in the geniwrapper module, are we are now moving to the sfa module

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -45,7 +45,7 @@ BootCD-SVNPATH			:= http://svn.planet-lab.org/svn/BootCD/tags/BootCD-4.2-14
 VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn/VserverReference/tags/VserverReference-4.2-12
 BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn/BootstrapFS/tags/BootstrapFS-1.0-8
 MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/trunk
-sfa-SVNPATH                     := http://svn.planet-lab.org/svn/geniwrapper/tags/geniwrapper-0.8-5
+sfa-SVNPATH                     := http://svn.planet-lab.org/svn/geniwrapper/tags/geniwrapper-0.8-6
 pyopenssl-SVNPATH               := http://svn.planet-lab.org/svn/pyopenssl/tags/pyopenssl-0.9-1
 PLCRT-SVNPATH			:= http://svn.planet-lab.org/svn/PLCRT/tags/PLCRT-1.0-5
 
