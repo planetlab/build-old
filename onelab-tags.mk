75b71f4a9b2e6e7ec8209eee3cdf4ff28800ddbd
Marc Fiuczynski <mef@cs.princeton.edu>
2009-11-03 21:13:12 +0000

    Setting tag PLCRT-1.0-11 Make sure to do updates to sendmail files, rather than appeneding same hostname values to the end of files.

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -46,7 +46,7 @@ BootstrapFS-SVNPATH		:= http://svn.planet-lab.org/svn/BootstrapFS/tags/Bootstrap
 MyPLC-SVNPATH			:= http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-4.3-28
 sfa-SVNPATH			:= http://svn.planet-lab.org/svn/sfa/tags/sfa-0.9-5
 pyopenssl-SVNPATH		:= http://svn.planet-lab.org/svn/pyopenssl/tags/pyopenssl-0.9-1
-PLCRT-SVNPATH			:= http://svn.planet-lab.org/svn/PLCRT/tags/PLCRT-1.0-10
+PLCRT-SVNPATH			:= http://svn.planet-lab.org/svn/PLCRT/tags/PLCRT-1.0-11
 
 # locating the right test directory - see make tests_svnpath
 tests-SVNPATH			:= http://svn.planet-lab.org/svn/tests/tags/tests-4.3-5
