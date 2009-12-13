06fac4137a7adab4e82ee4929a19fdad6d729f5b
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2009-12-13 22:31:23 +0000

    first draft of the f12 distro

---
+++ f12-tags.mk
@@ -0,0 +1,9 @@
+#
+# $Id: onelab-tags.mk 15918 2009-12-01 08:02:26Z thierry $
+# $URL$
+#
+
+linux-2.6-SVNBRANCH		:= f12
+linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/branches/f12
+util-vserver-SVNBRANCH		:= f12
+util-vserver-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver/branches/f12
