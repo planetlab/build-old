637ec7a8f3614068d25d10eac1ac4b36feb6f4f1
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2009-12-15 08:50:41 +0000

    use revisions in tags

--- f12-tags.mk
+++ f12-tags.mk
@@ -4,8 +4,8 @@
 #
 
 linux-2.6-SVNBRANCH		:= f12
-linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/branches/f12
+linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/branches/f12@16162
 util-vserver-SVNBRANCH		:= f12
-util-vserver-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver/branches/f12
+util-vserver-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver/branches/f12@16162
 yum-BRANCH			:= f12
-yum-SVNPATH			:= http://svn.planet-lab.org/svn/yum/branches/f12
+yum-SVNPATH			:= http://svn.planet-lab.org/svn/yum/branches/f12@16162
