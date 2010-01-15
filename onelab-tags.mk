3e6175bf22d20eac793242977c73365b937b9c4e
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-01-15 08:54:46 +0000

    tests trunk

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -51,4 +51,4 @@ pyopenssl-SVNPATH		:= http://svn.planet-lab.org/svn/pyopenssl/tags/pyopenssl-0.9
 PLCRT-SVNPATH			:= http://svn.planet-lab.org/svn/PLCRT/tags/PLCRT-1.0-11
 
 # locating the right test directory - see make tests_svnpath
-tests-SVNPATH                   := http://svn.planet-lab.org/svn/tests/tags/tests-4.3-6
+tests-SVNPATH                   := http://svn.planet-lab.org/svn/tests/trunk
