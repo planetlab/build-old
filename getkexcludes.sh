05456a2c2c049a03bbf0738a813734bb07616604
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-01-27 18:44:46 +0000

    fixed and moved the server-side yum scripts, from nodeconfig/yum to BootstrapFS/nodeconfig/yum   now known as the nodeyum package   (might need some moer work for properly handling kexcludes in multi-fc plcs) cleaned up all uses of /etc/planetlab/{node,slice}family nodeconfig now does not need the build/ module anymore at build time

--- getkexcludes.sh
+++ getkexcludes.sh
@@ -4,13 +4,29 @@ COMMAND=$(basename $0)
 . $(dirname $0)/build.common
 
 function usage () {
-    echo "Usage: $COMMAND"
+    echo "Usage: $COMMAND [-f fcdistro]"
     echo "outputs the list of packages to exclude from the stock repositories"
     echo "this is set in build.common, and needs to fit the set of packages"
     echo "that we override in the planetlab build"
     exit 1
 }
 
+while getopts "f:" opt ; do
+    case $opt in
+	f) FCDISTRO=$OPTARG ;;
+	h|*) usage ;;
+    esac
+done
+	
+toshift=$(($OPTIND - 1))
+shift $toshift
+
 [[ -n "$@" ]] && usage
 
-echo "$pl_KEXCLUDES"
+# if the fcdistro is passed in argument
+if [ -n "$FCDISTRO" ] ; then
+    pl_getKexcludes "$FCDISTRO"
+# otherwise use the value for the current system
+else
+    echo "$pl_KEXCLUDES"
+fi
