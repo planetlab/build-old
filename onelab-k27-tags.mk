60cc677f3189f69842e91c9ecb0ef97d791a519e
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-01-28 09:45:34 +0000

    use vserver-reference trunk

--- onelab-k27-tags.mk
+++ onelab-k27-tags.mk
@@ -44,7 +44,7 @@ nodeconfig-SVNPATH		:= http://svn.planet-lab.org/svn/nodeconfig/trunk
 BootManager-SVNPATH             := http://svn.planet-lab.org/svn/BootManager/trunk
 pypcilib-SVNPATH                := http://svn.planet-lab.org/svn/pypcilib/tags/pypcilib-0.2-9
 BootCD-SVNPATH                  := http://svn.planet-lab.org/svn/BootCD/trunk
-VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn/VserverReference/tags/VserverReference-4.2-16
+VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn/VserverReference/trunk
 BootstrapFS-SVNPATH             := http://svn.planet-lab.org/svn/BootstrapFS/trunk
 # latest trunk do not need build anymore
 myplc-MODULES := MyPLC
