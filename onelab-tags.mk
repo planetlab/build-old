68760c87aedf647284c0a63d87c65cc6a8e22f33
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-01-29 14:32:23 +0000

    monitor vserverreference branch 4.2

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -48,6 +48,7 @@ BootManager-SVNPATH             := http://svn.planet-lab.org/svn/BootManager/tag
 pypcilib-SVNPATH                := http://svn.planet-lab.org/svn/pypcilib/tags/pypcilib-0.2-9
 BootCD-SVNBRANCH		:= 4.2
 BootCD-SVNPATH                  := http://svn.planet-lab.org/svn/BootCD/tags/BootCD-4.2-17
+VserverReference-SVNBRANCH	:= 4.2
 VserverReference-SVNPATH	:= http://svn.planet-lab.org/svn/VserverReference/tags/VserverReference-4.2-16
 BootstrapFS-SVNBRANCH		:= 1.0
 BootstrapFS-SVNPATH             := http://svn.planet-lab.org/svn/BootstrapFS/tags/BootstrapFS-1.0-11
