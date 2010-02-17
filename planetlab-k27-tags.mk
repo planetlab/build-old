a3bcc7015ff6befd07aae236a24c380708bbc4c3
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-02-17 20:39:51 +0000

    use k27 compatible madwifi

--- planetlab-k27-tags.mk
+++ planetlab-k27-tags.mk
@@ -7,7 +7,7 @@
 # VINI is running a prototype of a 2.6.27-based PlanetLab node (aka Trellis)
 # Below we include a few Trellis versions of packages 
 linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-27-6
-madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-3878-0
+madwifi-SVNPATH                 := http://svn.planet-lab.org/svn/madwifi/tags/madwifi-4099-0
 # Trellis is using a modified util-vserver and util-vserver-pl with the 2.6.27 kernel
 # this branch is numbered 0.30.216-pre2833 in the specfile
 # initial tag would be util-vserver-0.30.216-pre2833-0
