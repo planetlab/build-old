73aa85b5bcc42ce586d16af5a25637cd308fc0dd
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-03-12 17:14:07 +0000

    use tests trunk

--- onelab-k27-tags.mk
+++ onelab-k27-tags.mk
@@ -48,7 +48,7 @@ BootstrapFS-SVNPATH             := http://svn.planet-lab.org/svn/BootstrapFS/tru
 MyPLC-SVNPATH                   := http://svn.planet-lab.org/svn/MyPLC/trunk
 
 # locating the right test directory - see make tests_svnpath
-tests-SVNPATH                   := http://svn.planet-lab.org/svn/tests/tags/tests-5.0-2
+tests-SVNPATH                   := http://svn.planet-lab.org/svn/tests/trunk
 
 ### temporary
 # nozomi not needed anymore for 2.6.27
--- onelab-tags.mk
+++ onelab-tags.mk
@@ -52,4 +52,4 @@ BootstrapFS-SVNPATH             := http://svn.planet-lab.org/svn/BootstrapFS/tru
 MyPLC-SVNPATH                   := http://svn.planet-lab.org/svn/MyPLC/trunk
 
 # locating the right test directory - see make tests_svnpath
-tests-SVNPATH                   := http://svn.planet-lab.org/svn/tests/tags/tests-5.0-2
+tests-SVNPATH                   := http://svn.planet-lab.org/svn/tests/trunk
