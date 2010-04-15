ff18a44300fd7b5a1d8391a60a6f30ebba5824c5
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-04-15 19:49:27 +0000

    Setting tag linux-2.6-22-47 Tagging 22 branch with following changes; * Make barrier=1 for ext3 filesystem, * Support barriers on single device dm devices, * Introduce various ext3 related fixes, * Fix conflict between glibc-headers kernel-headers packages

--- planetlab-tags.mk
+++ planetlab-tags.mk
@@ -4,7 +4,7 @@
 # build-SVNPATH is now set by vbuild-nightly.sh to avoid duplication
 
 linux-2.6-SVNBRANCH		:= 22
-linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-22-46
+linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-22-47
 madwifi-SVNBRANCH		:= 0.9.4
 madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-0.9.4-3
 iptables-SVNBRANCH		:= 1.3.8
