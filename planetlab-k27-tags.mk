4ff6deb6291c4946db07b9c4f479f5f9b8a218af
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-04-19 18:26:16 +0000

    use trunk for testing

--- planetlab-k27-tags.mk
+++ planetlab-k27-tags.mk
@@ -3,7 +3,7 @@
 
 # build-SVNPATH is now set by vbuild-nightly.sh to avoid duplication
 
-linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-27-8
+linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/trunk
 madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-4099-0
 iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.4.7-2
 iproute2-SVNPATH		:= http://svn.planet-lab.org/svn/iproute2/tags/iproute2-2.6.33-2
