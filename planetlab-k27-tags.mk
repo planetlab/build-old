ff2492c36639dcaa3272cdea2430ec7d979ac634
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-05-03 19:50:59 +0000

    Setting tag iptables-1.4.7-3 drop backward compatibility with older iptables versions as new kernels only support v2 remove unused modules from iptables-config file

--- planetlab-k27-tags.mk
+++ planetlab-k27-tags.mk
@@ -5,7 +5,7 @@
 
 linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-27-9
 madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-4099-0
-iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.4.7-2
+iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.4.7-3
 iptables-BUILD-FROM-SRPM := yes	# tmp
 iproute2-SVNPATH		:= http://svn.planet-lab.org/svn/iproute2/tags/iproute2-2.6.33-2
 iproute-BUILD-FROM-SRPM := yes	# tmp
