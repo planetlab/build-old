bcd7f1bd06f3d43fde9d320c9cd25cbe1a05e160
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-05-10 20:29:36 +0000

    use new tag

--- planetlab-k32-tags.mk
+++ planetlab-k32-tags.mk
@@ -14,8 +14,7 @@ util-vserver-BUILD-FROM-SRPM := yes	# tmp
 libnl-SVNPATH			:= http://svn.planet-lab.org/svn/libnl/tags/libnl-1.1-2
 # Trellis is using a modified util-vserver-pl with the 2.6.27 kernel
 # initial tag would be tags/util-vserver-pl-0.4-5
-util-vserver-pl-SVNBRANCH	:= trellis
-util-vserver-pl-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver-pl/branches/trellis
+util-vserver-pl-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver-pl/tags/util-vserver-pl-0.4-5
 NodeUpdate-SVNPATH		:= http://svn.planet-lab.org/svn/NodeUpdate/tags/NodeUpdate-0.5-6
 PingOfDeath-SVNPATH		:= http://svn.planet-lab.org/svn/PingOfDeath/tags/PingOfDeath-2.2-1
 NodeManager-SVNPATH		:= http://svn.planet-lab.org/svn/NodeManager/tags/NodeManager-2.0-6
