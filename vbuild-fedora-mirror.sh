bd818e79b66a1bc9804acee929358562223be84a
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-05-17 15:14:27 +0000

    deprecating updates of f10 and centos5.3 as part of the -F option

--- vbuild-fedora-mirror.sh
+++ vbuild-fedora-mirror.sh
@@ -36,7 +36,7 @@ jp_centos_url="jp_centos_url-needs-to-be-defined"
 jp_epel_url="jp_epel_url-needs-to-be-defined"
 
 default_distroname="centos5.5"
-all_distronames="f10 f12 centos5.3 centos5.4 centos5.5 epel5"
+all_distronames="f12 centos5.4 centos5.5 epel5"
 default_arch="i386"
 all_archs="i386 x86_64"
 
