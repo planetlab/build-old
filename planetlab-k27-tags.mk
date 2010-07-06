362ac84de19703e262091be4de17899d053a4b5c
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-07-05 21:08:35 -0400

    Setting tag linux-2.6-27-15 Bump to 2.6.27.48

--- planetlab-k27-tags.mk
+++ planetlab-k27-tags.mk
@@ -1,6 +1,6 @@
 # build-GITPATH is now set by vbuild-nightly.sh to avoid duplication
 
-linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-27-14
+linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/tags/linux-2.6-27-15
 madwifi-SVNPATH			:= http://svn.planet-lab.org/svn/madwifi/tags/madwifi-4099-0
 iptables-SVNPATH		:= http://svn.planet-lab.org/svn/iptables/tags/iptables-1.4.8-0
 iptables-BUILD-FROM-SRPM := yes	# tmp
