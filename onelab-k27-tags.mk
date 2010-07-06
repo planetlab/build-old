1dc7475e3fdce1ff138ef184f046de1a6b3e7293
Barış Metin <Talip-Baris.Metin@sophia.inria.fr>
2010-07-06 17:59:05 +0200

    Setting tag MyPLC-5.0-7 disable mod_wsgi

--- onelab-k27-tags.mk
+++ onelab-k27-tags.mk
@@ -50,7 +50,7 @@ pyplnet-SVNPATH			:= http://svn.planet-lab.org/svn/pyplnet/tags/pyplnet-4.3-6
 BootCD-SVNPATH                  := http://svn.planet-lab.org/svn/BootCD/tags/BootCD-5.0-4
 VserverReference-SVNPATH        := http://svn.planet-lab.org/svn/VserverReference/tags/VserverReference-5.0-3
 BootstrapFS-SVNPATH             := http://svn.planet-lab.org/svn/BootstrapFS/tags/BootstrapFS-2.0-6
-MyPLC-SVNPATH                   := http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-5.0-6
+MyPLC-SVNPATH                   := http://svn.planet-lab.org/svn/MyPLC/tags/MyPLC-5.0-7
 DistributedRateLimiting-SVNPATH	:= http://svn.planet-lab.org/svn/DistributedRateLimiting/tags/DistributedRateLimiting-0.1-1
 
 # locating the right test directory - see make tests_gitpath
