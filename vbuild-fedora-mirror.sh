28fa97f4327e728c5a30116b3dc7c0812a3cbe44
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-11-18 11:46:40 +0100

    getting ready for fedora 14

--- vbuild-fedora-mirror.sh
+++ vbuild-fedora-mirror.sh
@@ -35,7 +35,7 @@ jp_centos_url="jp_centos_url-needs-to-be-defined"
 jp_epel_url="jp_epel_url-needs-to-be-defined"
 
 default_distroname="centos5.5"
-all_distronames="f13 f12 centos5.4 centos5.5 epel5"
+all_distronames="f14 f12 centos5.4 centos5.5 epel5"
 default_arch="i386"
 all_archs="i386 x86_64"
 
