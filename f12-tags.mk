cf410ff51d5bc1760c62765f80d7059c7b66dd88
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-11-30 10:38:51 +0100

    try out f12 branch for pre2925

--- f12-tags.mk
+++ f12-tags.mk
@@ -1,6 +1,6 @@
 linux-2.6-BRANCH		:= f12
-linux-2.6-SVNPATH		:= http://svn.planet-lab.org/svn/linux-2.6/branches/f12@16162
+linux-2.6-GITPATH		:= http://git.onelab.eu/svn/linux-2.6@f12
 util-vserver-BRANCH		:= f12
-util-vserver-SVNPATH		:= http://svn.planet-lab.org/svn/util-vserver/branches/f12@16162
+util-vserver-GITPATH		:= http://git.onelab.eu/svn/util-vserver@f12
 yum-BRANCH			:= f12
-yum-SVNPATH			:= http://svn.planet-lab.org/svn/yum/branches/f12@16162
+yum-SVNPATH			:= http://svn.planet-lab.org/svn/yum/branches/f12
