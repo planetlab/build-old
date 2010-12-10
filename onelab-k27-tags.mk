6e3b86e56dd2c16974877da9196e620fe565c3a8
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-12-10 13:37:56 -0500

    Setting tag linux-2.6-27-33 Bump to 2.6.27.57

--- onelab-k27-tags.mk
+++ onelab-k27-tags.mk
@@ -1,7 +1,7 @@
 # build-GITPATH is now set by vbuild-nightly.sh to avoid duplication
 
 ###
-linux-2.6-GITPATH		:= git://git.onelab.eu/linux-2.6.git@linux-2.6-27-32
+linux-2.6-GITPATH		:= git://git.onelab.eu/linux-2.6.git@linux-2.6-27-33
 madwifi-GITPATH			:= git://git.onelab.eu/madwifi.git@madwifi-4132-1
 iptables-GITPATH                := git://git.onelab.eu/iptables.git@iptables-1.4.9-0
 iptables-BUILD-FROM-SRPM        := yes	# tmp
