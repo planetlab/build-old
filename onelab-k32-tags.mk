829f823d80816f18d6f9f6164340e05cd1c7f49e
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-12-21 16:23:23 -0500

    use new tag

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -41,7 +41,7 @@ PLCRT-SVNPATH			:= http://svn.planet-lab.org/svn/PLCRT/tags/PLCRT-1.0-11
 pyopenssl-SVNPATH		:= http://svn.planet-lab.org/svn/pyopenssl/tags/pyopenssl-0.9-1
 ###
 pyaspects-GITPATH		:= git://git.onelab.eu/pyaspects.git@pyaspects-0.4.1-0
-ejabberd-GITPATH		:= git://git.onelab.eu/ejabberd.git@ejabberd-2.1.3-2
+ejabberd-GITPATH		:= git://git.onelab.eu/ejabberd.git@ejabberd-2.1.6-1
 omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-8
 ###
 sfa-GITPATH                     := git://git.onelab.eu/sfa.git@master
