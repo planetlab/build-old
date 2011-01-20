2547de11850a35fd7722f1355cc8919a65a93ff0
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-01-20 11:44:29 +0100

    openvswitch seems to require kernel-devel installed at build time

--- onelab.mk
+++ onelab.mk
@@ -263,6 +263,7 @@ endif
 #
 openvswitch-MODULES := openvswitch
 openvswitch-SPEC := openvswitch.spec
+openvswitch-DEPEND-DEVEL-RPMS := kernel-devel
 IN_BOOTSTRAPFS += openvswitch
 ALL += openvswitch
 
