cb4156bc79be3dbc3a9b81dca1dc24f00df47e3a
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-01-23 20:32:27 +0100

    Setting tag nodeconfig-5.0-4 trashed sfa_component_setup job & conf file

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -48,7 +48,7 @@ omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-8
 ###
 sfa-GITPATH                     := git://git.onelab.eu/sfa.git@master
 sface-GITPATH                   := git://git.onelab.eu/sface.git@sface-0.1-3
-nodeconfig-GITPATH		:= git://git.onelab.eu/nodeconfig.git@master
+nodeconfig-GITPATH              := git://git.onelab.eu/nodeconfig.git@nodeconfig-5.0-4
 bootmanager-GITPATH             := git://git.onelab.eu/bootmanager.git@bootmanager-5.0-16
 pypcilib-GITPATH		:= git://git.onelab.eu/pypcilib.git@pypcilib-0.2-9
 pyplnet-GITPATH			:= git://git.onelab.eu/pyplnet.git@pyplnet-4.3-7
