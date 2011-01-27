c25f9df155fea291c70b56727ed76e988a3a1a46
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-01-27 22:02:03 +0100

    Setting tag sface-0.1-4 minor changes

--- onelab-k27-tags.mk
+++ onelab-k27-tags.mk
@@ -44,7 +44,7 @@ ejabberd-GITPATH		:= git://git.onelab.eu/ejabberd.git@ejabberd-2.1.3-2
 omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-9
 ###
 sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-1.0-12
-sface-GITPATH                   := git://git.onelab.eu/sface.git@sface-0.1-3
+sface-GITPATH                   := git://git.onelab.eu/sface.git@sface-0.1-4
 nodeconfig-GITPATH              := git://git.onelab.eu/nodeconfig.git@nodeconfig-5.0-4
 bootmanager-GITPATH             := git://git.onelab.eu/bootmanager.git@bootmanager-5.0-16
 pypcilib-GITPATH		:= git://git.onelab.eu/pypcilib.git@pypcilib-0.2-9
