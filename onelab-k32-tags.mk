5eb2b260d83431913e316d70180559996d8bbb40
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-02-01 19:40:07 +0100

    Setting tag omf-5.3-10 move to more recent releases of omf-resctl & related stuff

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -45,7 +45,7 @@ pyopenssl-SVNPATH		:= http://svn.planet-lab.org/svn/pyopenssl/tags/pyopenssl-0.9
 ###
 pyaspects-GITPATH		:= git://git.onelab.eu/pyaspects.git@pyaspects-0.4.1-1
 ejabberd-GITPATH		:= git://git.onelab.eu/ejabberd.git@ejabberd-2.1.6-2
-omf-GITPATH                     := git://git.onelab.eu/omf.git@master
+omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-10
 oml-GITPATH                     := git://git.onelab.eu/oml.git@master
 ###
 sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-1.0-12
