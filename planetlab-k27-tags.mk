e6465d06c77ac5e426a64b363552d853953ecd93
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-02-02 09:29:20 +0100

    Setting tag tests-5.0-23 only new email address

--- planetlab-k27-tags.mk
+++ planetlab-k27-tags.mk
@@ -50,4 +50,4 @@ vserver-reference-GITPATH       := git://git.planet-lab.org/vserver-reference.gi
 bootstrapfs-GITPATH             := git://git.planet-lab.org/bootstrapfs.git@bootstrapfs-2.0-8
 myplc-GITPATH                   := git://git.planet-lab.org/myplc.git@myplc-5.0-14
 # locating the right test directory - see make tests_gitpath
-tests-GITPATH                   := git://git.planet-lab.org/tests.git@tests-5.0-22
+tests-GITPATH                   := git://git.planet-lab.org/tests.git@tests-5.0-23
