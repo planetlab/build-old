ef3b070d09991aa9e70d39a9fe8100bbfe0e85e0
Andy Bavier <acb@cs.princeton.edu>
2011-03-08 11:09:54 -0500

    Setting tag sfa-1.0-16 Fix build problem First version of SFA scanner

--- planetlab-k27-tags.mk
+++ planetlab-k27-tags.mk
@@ -38,7 +38,7 @@ pyaspects-GITPATH		:= git://git.planet-lab.org/pyaspects.git@pyaspects-0.4.1-1
 ejabberd-GITPATH		:= git://git.planet-lab.org/ejabberd.git@ejabberd-2.1.3-2
 omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-9
 ###
-sfa-GITPATH			:= git://git.planet-lab.org/sfa.git@sfa-1.0-15
+sfa-GITPATH			:= git://git.planet-lab.org/sfa.git@sfa-1.0-16
 nodeconfig-GITPATH		:= git://git.planet-lab.org/nodeconfig.git@nodeconfig-5.0-5
 pypcilib-GITPATH		:= git://git.planet-lab.org/pypcilib.git@pypcilib-0.2-9
 pyplnet-GITPATH                 := git://git.planet-lab.org/pyplnet.git@pyplnet-4.3-9
