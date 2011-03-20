38bf69b42ccce248f10cdfa187a6e5658a63e19a
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-03-21 00:53:06 +0100

    Setting tag tests-5.0-25 test new iniscript_body feature

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -63,7 +63,7 @@ myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.0-16
 DistributedRateLimiting-SVNPATH	:= http://svn.planet-lab.org/svn/DistributedRateLimiting/tags/DistributedRateLimiting-0.1-1
 
 # locating the right test directory - see make tests_gitpath
-tests-GITPATH                   := git://git.onelab.eu/tests.git@master
+tests-GITPATH                   := git://git.onelab.eu/tests.git@tests-5.0-25
 
 ### temporary
 # nozomi not needed anymore for 2.6.27
