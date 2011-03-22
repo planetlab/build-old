a61476482816ac776e3230096f9c2f6fc567c3cc
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-03-22 13:28:47 +0100

    Setting tag tests-5.0-26 rename initscript_body into initscript_code run sfa unittest in a single step, ignore failure to yum install for f14/i386

--- planetlab-k27-tags.mk
+++ planetlab-k27-tags.mk
@@ -50,4 +50,4 @@ vserver-reference-GITPATH       := git://git.planet-lab.org/vserver-reference.gi
 bootstrapfs-GITPATH             := git://git.planet-lab.org/bootstrapfs.git@bootstrapfs-2.0-10
 myplc-GITPATH                   := git://git.planet-lab.org/myplc.git@myplc-5.0-17
 # locating the right test directory - see make tests_gitpath
-tests-GITPATH                   := git://git.planet-lab.org/tests.git@tests-5.0-25
+tests-GITPATH                   := git://git.planet-lab.org/tests.git@tests-5.0-26
