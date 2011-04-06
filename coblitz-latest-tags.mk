808d22b15a5403f90d7a37dc2593c2a6cdf987ed
S.Çağlar Onur <caglar@verivue.com>
2011-04-06 17:15:21 -0400

    sync with master

--- coblitz-latest-tags.mk
+++ coblitz-latest-tags.mk
@@ -10,7 +10,7 @@ util-vserver-pl-GITPATH         := git://git.verivue.com/planetlab/util-vserver-
 libnl-SVNPATH			:= http://svn.planet-lab.org/svn/libnl/tags/libnl-1.1-2
 nodeupdate-GITPATH		:= git://git.verivue.com/planetlab/nodeupdate.git@nodeupdate-0.5-8
 nodemanager-GITPATH		:= git://git.verivue.com/planetlab/nodemanager@nodemanager-1.8-38
-pyplnet-GITPATH                 := git://git.verivue.com/planetlab/pyplnet@pyplnet-4.3-9
+pyplnet-GITPATH                 := git://git.verivue.com/planetlab/pyplnet@pyplnet-4.3-10
 codemux-GITPATH                 := git://git.verivue.com/planetlab/codemux.git@CoDemux-0.1-13
 iptables-BUILD-FROM-SRPM        := yes # tmp
 iptables-GITPATH                := git://git.verivue.com/planetlab/iptables.git@iptables-1.4.10-4
@@ -31,7 +31,7 @@ pypcilib-GITPATH                := git://git.verivue.com/planetlab/pypcilib.git@
 bootcd-GITPATH                  := git://git.verivue.com/planetlab/bootcd.git@bootcd-4.2-25
 vserver-reference-GITPATH	:= git://git.verivue.com/planetlab/vserver-reference@vserver-reference-4.2-18
 bootstrapfs-GITPATH             := git://git.verivue.com/planetlab/bootstrapfs.git@BootstrapFS-1.0-12
-myplc-GITPATH                   := git://git.verivue.com/planetlab/myplc@myplc-4.3-47
+myplc-GITPATH                   := git://git.verivue.com/planetlab/myplc@myplc-4.3-48
 pyopenssl-SVNPATH		:= http://svn.planet-lab.org/svn/pyopenssl/tags/pyopenssl-0.9-1
 
 # locating the right test directory - see make tests_gitpath
