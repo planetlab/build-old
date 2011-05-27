b52fbc991a79d76ab9c6c80a90c5d12db892e7a0
Stephen Soltesz <soltesz@cs.princeton.edu>
2011-05-27 19:38:20 -0400

    Build monitor from git

--- planetlab-k32-tags.mk
+++ planetlab-k32-tags.mk
@@ -37,7 +37,7 @@ plcapi-GITPATH                  := git://git.planet-lab.org/plcapi@plcapi-5.0-32
 drupal-SVNPATH			:= http://svn.planet-lab.org/svn/drupal/tags/drupal-4.7-14
 plewww-GITPATH			:= git://git.planet-lab.org/plewww@plewww-4.3-63
 www-register-wizard-SVNPATH	:= http://svn.planet-lab.org/svn/www-register-wizard/tags/www-register-wizard-4.3-5
-Monitor-SVNPATH			:= http://svn.planet-lab.org/svn/Monitor/tags/Monitor-3.0-35
+monitor-GITPATH			:= git://git.planet-lab.org/monitor@monitor-3.1-2
 PLCRT-SVNPATH			:= http://svn.planet-lab.org/svn/PLCRT/tags/PLCRT-1.0-11
 pyopenssl-SVNPATH		:= http://svn.planet-lab.org/svn/pyopenssl/tags/pyopenssl-0.9-1
 ###
