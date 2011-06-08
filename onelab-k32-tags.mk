dba12dc694d3474b0b8fc70b62b7ed012d6e10ee
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-06-08 21:07:27 +0200

    try k32 kernel from master

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -2,7 +2,7 @@
 
 ###
 linux-2.6-BRANCH		:= rhel6
-linux-2.6-GITPATH		:= git://git.onelab.eu/linux-2.6.git@linux-2.6-32-20
+linux-2.6-GITPATH               := git://git.onelab.eu/linux-2.6.git@master
 # help out spec2make on f8 and centos5, due to a bug in rpm
 ifeq "$(DISTRONAME)" "$(filter $(DISTRONAME),f8 centos5)"
 kernel-WHITELIST-RPMS	:= kernel-devel,kernel-headers
