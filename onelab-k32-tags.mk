4d6e24d37a563f48857027730157e0c80dfe6d74
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-06-09 09:47:50 +0200

    whoops - wrong head

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -2,7 +2,7 @@
 
 ###
 linux-2.6-BRANCH		:= rhel6
-linux-2.6-GITPATH               := git://git.onelab.eu/linux-2.6.git@master
+linux-2.6-GITPATH               := git://git.onelab.eu/linux-2.6.git@rhel6
 # help out spec2make on f8 and centos5, due to a bug in rpm
 ifeq "$(DISTRONAME)" "$(filter $(DISTRONAME),f8 centos5)"
 kernel-WHITELIST-RPMS	:= kernel-devel,kernel-headers
