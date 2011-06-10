40db08bc141631918b12cc4757e3dc74fc892030
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-06-10 15:11:54 +0200

    Setting tag bootmanager-5.0-19 nicer log - was intended for previous tag

--- planetlab-k32-tags.mk
+++ planetlab-k32-tags.mk
@@ -44,7 +44,7 @@ omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-10
 ###
 sfa-GITPATH                     := git://git.planet-lab.org/sfa.git@sfa-1.0-23
 nodeconfig-GITPATH		:= git://git.planet-lab.org/nodeconfig.git@nodeconfig-5.0-5
-bootmanager-GITPATH             := git://git.planet-lab.org/bootmanager.git@bootmanager-5.0-18
+bootmanager-GITPATH             := git://git.planet-lab.org/bootmanager.git@bootmanager-5.0-19
 pypcilib-GITPATH		:= git://git.planet-lab.org/pypcilib.git@pypcilib-0.2-10
 pyplnet-GITPATH                 := git://git.planet-lab.org/pyplnet.git@pyplnet-4.3-9
 DistributedRateLimiting-SVNPATH	:= http://svn.planet-lab.org/svn/DistributedRateLimiting/tags/DistributedRateLimiting-0.1-1
