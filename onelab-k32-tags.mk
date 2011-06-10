f7e6b3d674117298a6ef33ea4dc96d8bec7bb3f2
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-06-10 13:13:46 +0200

    Setting tag sface-0.1-14 slight changes in view creation

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -48,7 +48,7 @@ omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-10
 oml-GITPATH                     := git://git.onelab.eu/oml.git@oml-2.5.1-1
 ###
 sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-1.0-23
-sface-GITPATH                   := git://git.onelab.eu/sface.git@sface-0.1-13
+sface-GITPATH                   := git://git.onelab.eu/sface.git@sface-0.1-14
 nodeconfig-GITPATH              := git://git.onelab.eu/nodeconfig.git@nodeconfig-5.0-5
 bootmanager-GITPATH             := git://git.onelab.eu/bootmanager.git@bootmanager-5.0-18
 pypcilib-GITPATH		:= git://git.onelab.eu/pypcilib.git@pypcilib-0.2-10
