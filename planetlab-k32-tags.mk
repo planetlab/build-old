0f078e8d92d079e323af02c969ad0a70862483aa
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-06-10 15:37:59 +0200

    Setting tag bootstrapfs-2.0-12 minor tweak in plc.d/packages

--- planetlab-k32-tags.mk
+++ planetlab-k32-tags.mk
@@ -51,7 +51,7 @@ DistributedRateLimiting-SVNPATH	:= http://svn.planet-lab.org/svn/DistributedRate
 pcucontrol-GITPATH              := git://git.planet-lab.org/pcucontrol.git@pcucontrol-1.0-11
 bootcd-GITPATH                  := git://git.planet-lab.org/bootcd.git@bootcd-5.0-10
 vserver-reference-GITPATH       := git://git.planet-lab.org/vserver-reference.git@vserver-reference-5.0-6
-bootstrapfs-GITPATH             := git://git.planet-lab.org/bootstrapfs.git@bootstrapfs-2.0-11
+bootstrapfs-GITPATH             := git://git.planet-lab.org/bootstrapfs.git@bootstrapfs-2.0-12
 myplc-GITPATH                   := git://git.planet-lab.org/myplc.git@myplc-5.0-18
 # locating the right test directory - see make tests_gitpath
 tests-GITPATH                   := git://git.planet-lab.org/tests.git@tests-5.0-27
