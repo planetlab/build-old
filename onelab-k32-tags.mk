e7126113b6e5584f418d36c768831616f4c64755
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-09-26 16:18:04 +0200

    Setting tag myplc-5.0-19 new maintenance/monitoring script spot-aliens to look for glitches in refreshpeer+sfa

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -58,7 +58,7 @@ pyplnet-GITPATH                 := git://git.onelab.eu/pyplnet.git@pyplnet-4.3-1
 bootcd-GITPATH                  := git://git.onelab.eu/bootcd.git@bootcd-5.0-10
 vserver-reference-GITPATH        := git://git.onelab.eu/vserver-reference.git@vserver-reference-5.0-6
 bootstrapfs-GITPATH             := git://git.onelab.eu/bootstrapfs.git@bootstrapfs-2.0-13
-myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.0-18
+myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.0-19
 DistributedRateLimiting-SVNPATH	:= http://svn.planet-lab.org/svn/DistributedRateLimiting/tags/DistributedRateLimiting-0.1-1
 
 # locating the right test directory - see make tests_gitpath
