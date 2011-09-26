99cf6f669a9dd526879fe4ea99a26722c173cfb5
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-09-26 10:12:33 +0200

    Setting tag linux-2.6-27-38 no functional change, just pull the patch file from our mirror

--- onelab-k27-tags.mk
+++ onelab-k27-tags.mk
@@ -1,7 +1,7 @@
 # build-GITPATH is now set by vbuild-nightly.sh to avoid duplication
 
 ###
-linux-2.6-GITPATH               := git://git.onelab.eu/linux-2.6.git@master
+linux-2.6-GITPATH               := git://git.onelab.eu/linux-2.6.git@linux-2.6-27-38
 madwifi-GITPATH                 := git://git.onelab.eu/madwifi.git@madwifi-4132-2
 iptables-GITPATH                := git://git.onelab.eu/iptables.git@iptables-1.4.10-5
 iproute2-GITPATH                := git://git.onelab.eu/iproute2.git@master
--- planetlab-k27-tags.mk
+++ planetlab-k27-tags.mk
@@ -1,6 +1,6 @@
 # build-GITPATH is now set by vbuild-nightly.sh to avoid duplication
 
-linux-2.6-GITPATH		:= git://git.planet-lab.org/linux-2.6.git@linux-2.6-27-37
+linux-2.6-GITPATH		:= git://git.planet-lab.org/linux-2.6.git@linux-2.6-27-38
 madwifi-GITPATH			:= git://git.planet-lab.org/madwifi.git@madwifi-4132-2
 iptables-GITPATH                := git://git.planet-lab.org/iptables.git@iptables-1.4.10-5
 iproute2-GITPATH                := git://git.planet-lab.org/iproute2.git@iproute2-2.6.37-2
