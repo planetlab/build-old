13da49f8ad86233c2e1c1288a3315ca250717240
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-10-04 07:43:15 +0200

    master for sfa and sface

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -49,8 +49,8 @@ ejabberd-GITPATH		:= git://git.onelab.eu/ejabberd.git@ejabberd-2.1.6-2
 omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-11
 oml-GITPATH                     := git://git.onelab.eu/oml.git@oml-2.6.1-1
 ###
-sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-1.0-36
-sface-GITPATH                   := git://git.onelab.eu/sface.git@sface-0.1-19
+sfa-GITPATH                     := git://git.onelab.eu/sfa.git@master
+sface-GITPATH                   := git://git.onelab.eu/sface.git@master
 nodeconfig-GITPATH              := git://git.onelab.eu/nodeconfig.git@nodeconfig-5.0-5
 bootmanager-GITPATH             := git://git.onelab.eu/bootmanager.git@bootmanager-5.0-20
 pypcilib-GITPATH		:= git://git.onelab.eu/pypcilib.git@pypcilib-0.2-10
