f1688cb6420239a9b1837f6962dddd22d364ce99
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-10-10 12:35:30 +0200

    oops - undo previous change, that aimed at k32 and *not* k27

--- onelab-k27-tags.mk
+++ onelab-k27-tags.mk
@@ -1,7 +1,7 @@
 # build-GITPATH is now set by vbuild-nightly.sh to avoid duplication
 
 ###
-linux-2.6-GITPATH               := git://git.onelab.eu/linux-2.6.git@linux-2.6-32-26
+linux-2.6-GITPATH               := git://git.onelab.eu/linux-2.6.git@linux-2.6-27-38
 madwifi-GITPATH                 := git://git.onelab.eu/madwifi.git@madwifi-4132-3
 iptables-GITPATH                := git://git.onelab.eu/iptables.git@iptables-1.4.10-5
 iproute2-GITPATH                := git://git.onelab.eu/iproute2.git@iproute2-2.6.37-3
