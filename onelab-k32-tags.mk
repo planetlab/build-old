4bee8ccf2d9bf0ae1c3ea1ac9fc7d9fb01ea40bd
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-11-07 10:27:58 +0100

    Setting tag linux-2.6-32-27 2 places for pulling sources from in case of network outage

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -2,7 +2,7 @@
 
 ###
 linux-2.6-BRANCH		:= rhel6
-linux-2.6-GITPATH               := git://git.onelab.eu/linux-2.6.git@rhel6
+linux-2.6-GITPATH               := git://git.onelab.eu/linux-2.6.git@linux-2.6-32-27
 # help out spec2make on f8 and centos5, due to a bug in rpm 
 # ditto on f15 for spec2make.py - tmp hopefully
 ifeq "$(DISTRONAME)" "$(filter $(DISTRONAME),f8 f15 centos5)"
