d3875dd22403758abf7fd0240d6fe2b5d680cd05
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-11-28 09:33:35 +0100

    Setting tag tests-5.0-31 Use flags SFA_DB* instead of SFA_PLC_DB* macro steps rewritten macro defined by a sequence (list) of steps all goes in a single file 'macros.py'

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -62,5 +62,5 @@ myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.0-19
 DistributedRateLimiting-SVNPATH	:= http://svn.planet-lab.org/svn/DistributedRateLimiting/tags/DistributedRateLimiting-0.1-1
 
 # locating the right test directory - see make tests_gitpath
-tests-GITPATH                   := git://git.onelab.eu/tests.git@master
+tests-GITPATH                   := git://git.onelab.eu/tests.git@tests-5.0-31
 
